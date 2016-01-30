704c1452ecafcc06a35db9e55061d3ebe03dd1ce
rsboggs <robertstephenboggs@gmail.com>
2016-01-29 20:36:40 -0800

    Update song queue view and song queue view entry

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,26 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: "table",
+
   initialize: function() {
+    this.render(); //renders when add or remove event fires from the song queue collection
   },
 
   render: function() {
-    return this.$el;
+    // return this.$el;
+    // // console.log("SQ THIS: ", this)
+    this.$el.children().detach();
+
+    this.$el.html('<th>Song Queue</th>').append(
+      this.collection.map(function(song) {
+        return new SongQueueEntryView({model: song}).render(); //this is totally like fake.
+      })
+    );
   }
 
+
 });
+
+
+  
